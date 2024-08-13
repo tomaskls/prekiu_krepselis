48014002ab081be405efb70352108e4bb7f25d64
tomaskls <tomaskls@hotmail.com>
2024-08-13 20:17:02 +0300

    turbut gerai

--- js/productDetails.js
+++ js/productDetails.js
@@ -3,6 +3,7 @@ export function productDetails(a, b) {
     if (!produktas) {
         return `Prekė, su ID: ${b} neegzistuoja.`
     }
+   
     let visoKaina = produktas.amount * produktas.unitPrice / 100
     let vntKaina = produktas.unitPrice / 100
     let line1 = '------------------------------------\n';
@@ -10,9 +11,9 @@ export function productDetails(a, b) {
         line1 += '------------------------------------\n';
         line1 += `ID          | ${produktas.id}\n`;
         line1 += `Pavadinimas | ${produktas.name}\n`;
-        line1 += `Kiekis      | ${produktas.amount}\n`;
-        line1 += `vnt kaina   | ${vntKaina.toFixed(2)}\n`;
-        line1 += `Viso moketi | ${visoKaina.toFixed(2)}\n`;
+        line1 += `Kiekis      | ${produktas.amount} vnt\n`;
+        line1 += `vnt kaina   | ${vntKaina.toFixed(2)} Eur\n`;
+        line1 += `Viso moketi | ${visoKaina.toFixed(2)} Eur\n`;
         line1 += '------------------------------------\n';
    return line1 
 }
